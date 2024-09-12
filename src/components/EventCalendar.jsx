484e6f28109452302438cc3602b8e1b5c110cef4
SrRiuz <mateo.jimenez@alternova.com>
2024-09-12 09:36:15 -0500

    Calendario

--- src/components/EventCalendar.jsx
+++ src/components/EventCalendar.jsx
@@ -8,10 +8,25 @@ const localizer = momentLocalizer(moment);
 
 const EventCalendar = () => {
   const [events, setEvents] = useState([]);
-
   const handleSelectSlot = ({ start, end }) => {
     const title = window.prompt('Nuevo evento');
     if (title) {
+      fetch("http://localhost/events/", {
+        method: "post",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ start, end, title })
+      })
+        .then(res => res.json())
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log("System errror")
+        })
+
       setEvents([...events, { start, end, title }]);
     }
   };
