23418a5685377d023b7818374c90906060fed7b7
SrRiuz <mateo.jimenez@alternova.com>
2024-10-03 08:26:43 -0500

    foo

--- src/components/EventCalendar.jsx
+++ src/components/EventCalendar.jsx
@@ -1,13 +1,24 @@
 // EventCalendar.js
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { useQuery } from 'react-query';
 
 const localizer = momentLocalizer(moment);
 
 const EventCalendar = () => {
   const [events, setEvents] = useState([]);
+  const { isLoading, data, isError } = useQuery("threads", () =>
+    fetch("http://localhost/events/").then(res => res.json())
+  );
+
+  useEffect(() => {
+    if (data) {
+      setEvents(() => data)
+    }
+  }, [data])
+
   const handleSelectSlot = ({ start, end }) => {
     const title = window.prompt('Nuevo evento');
     if (title) {
@@ -20,9 +31,6 @@ const EventCalendar = () => {
         body: JSON.stringify({ start, end, title })
       })
         .then(res => res.json())
-        .then(res => {
-          console.log(res)
-        })
         .catch(err => {
           console.log("System errror")
         })
